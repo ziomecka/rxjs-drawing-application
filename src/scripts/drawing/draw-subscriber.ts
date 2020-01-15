3d23c686347fb6dd9739c75f7ecdb2c766713438
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2020-01-15 22:12:32 +0100

    Add destructuring to drawObservable

--- src/scripts/drawing/draw-subscriber.ts
+++ src/scripts/drawing/draw-subscriber.ts
@@ -3,32 +3,44 @@ const adjust = (value: number) => value - 20;
 
 export const drawSubscriber = ([
   path,
-  [mousePosition, prevMousePosition],
+  [
+    { clientX: prevClientX, clientY: prevClientY } = {
+      clientX: null,
+      clientY: null,
+    },
+    { clientX, clientY } = {
+      clientX: null,
+      clientY: null,
+    },
+  ],
 ]: DrawSubscriberOptions) => {
-  if (path && mousePosition) {
-    const currentDAttribute = path.getAttribute('d');
-    const { clientX, clientY } = mousePosition;
+  if (!path || !clientX || !clientY) {
+    return;
+  }
 
-    if (prevMousePosition) {
-      const { clientX: prevClientX, clientY: prevClientY } = prevMousePosition;
-      path.setAttribute(
-        'd',
-        currentDAttribute
-          ? `${currentDAttribute} L ${adjust(clientX)} ${adjust(clientY)}`
-          // eslint-disable-next-line max-len
-          : `M ${adjust(prevClientX)} ${adjust(prevClientY)} L ${adjust(clientX)} ${adjust(clientY)}`,
-      );
-      return;
-    }
+  const currentDAttribute = path.getAttribute('d');
 
+  if (prevClientX && prevClientY) {
     path.setAttribute(
       'd',
       currentDAttribute
         ? `${currentDAttribute} L ${adjust(clientX)} ${adjust(clientY)}`
-        : `M ${adjust(clientX)} ${adjust(clientY)}`,
+        : `M ${adjust(prevClientX)} ${adjust(prevClientY)} L ${adjust(
+            clientX,
+          )} ${adjust(clientY)}`,
     );
+    return;
   }
+
+  path.setAttribute(
+    'd',
+    currentDAttribute
+      ? `${currentDAttribute} L ${adjust(clientX)} ${adjust(clientY)}`
+      : `M ${adjust(clientX)} ${adjust(clientY)}`,
+  );
 };
 
-type DrawSubscriberOptions =
-  [SVGPathElement | null, [MousePosition, MousePosition]];
+type DrawSubscriberOptions = [
+  SVGPathElement | null,
+  [MousePosition, MousePosition],
+];
